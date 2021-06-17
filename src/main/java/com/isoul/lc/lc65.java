d278d0c3853de88bd870db2c70ee9c14b8c06358
isoul <isoulne@163.com>
2021-06-17 21:32:13 +0800

    IDEA deploy.

--- src/main/java/com/isoul/lc/lc65.java
+++ src/main/java/com/isoul/lc/lc65.java
@@ -7,52 +7,66 @@ import java.util.Map;
  * @author isoul
  * @date 2021/6/17 19:53
  * @note Copyright (c) 2021 isoul. All rights reserved.
+ *
+ * 有限状态自动机，本题通过构建一个状态枚举和一个字符枚举，用Map把他们联系起来，
+ * 形成state-chartype-state的联系，通过当前字符的state来判断下一个字符是否合法（在chartype内）
+ * 用String.charAt()遍历字符，最后一个字符直接用状态判断并返回结果（boolean类型）
+ *
  */
 public class lc65 {
 
     public boolean isNumber(String s) {
 
         Map<State, Map<CharType, State>> transfer = new HashMap<State, Map<CharType, State>>();
+
         Map<CharType, State> initialMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
             put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
             put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
         }};
         transfer.put(State.STATE_INITIAL, initialMap);
+
         Map<CharType, State> intSignMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
             put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
         }};
         transfer.put(State.STATE_INT_SIGN, intSignMap);
+
         Map<CharType, State> integerMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
             put(CharType.CHAR_EXP, State.STATE_EXP);
             put(CharType.CHAR_POINT, State.STATE_POINT);
         }};
         transfer.put(State.STATE_INTEGER, integerMap);
+
         Map<CharType, State> pointMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
             put(CharType.CHAR_EXP, State.STATE_EXP);
         }};
         transfer.put(State.STATE_POINT, pointMap);
+
         Map<CharType, State> pointWithoutIntMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
         }};
         transfer.put(State.STATE_POINT_WITHOUT_INT, pointWithoutIntMap);
+
         Map<CharType, State> fractionMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
             put(CharType.CHAR_EXP, State.STATE_EXP);
         }};
         transfer.put(State.STATE_FRACTION, fractionMap);
+
         Map<CharType, State> expMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
             put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
         }};
         transfer.put(State.STATE_EXP, expMap);
+
         Map<CharType, State> expSignMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
         }};
         transfer.put(State.STATE_EXP_SIGN, expSignMap);
+
         Map<CharType, State> expNumberMap = new HashMap<CharType, State>() {{
             put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
         }};
@@ -109,7 +123,7 @@ public class lc65 {
 
     static class test {
         public static void main(String[] args) {
-            System.out.println(new lc65().isNumber("abc"));
+            System.out.println(new lc65().isNumber("123"));
         }
     }
 
